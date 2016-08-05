335ba4ef2c5804c1800c641bd5a321232a337f27
Conny <kawohl.bentodesign@gmail.com>
2016-08-05 18:03:22 +0200

    adds tagcloud.js

--- tagcloud.js
+++ tagcloud.js
@@ -1,9 +1,26 @@
 import React from 'react';
  
 class Tagcloud extends React.Component {
+  constructor(){
+    super();
+    this.state = { tag: 'Hello!' }
+  }
   render() {
-    return <p>I am a Tag!! Obviously.</p>;
+    return <div className="tagcloud_container">
+          <p>I am a tag: {this.state.tag}</p>
+
+          </div>
   }
 }
+
+var AddNewTag = React.createClass({
+  render: function() {
+    return (
+      <div>
+      <label>Create new Tag</label><input>Tag Name</input>
+    </div>
+    );
+  }
+});
  
 export default Tagcloud
