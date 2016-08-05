0c1e50a2c994e92d9f0b195a7216ec980619e27e
Conny <kawohl.bentodesign@gmail.com>
2016-08-05 17:44:00 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Tagcloud from './tagcloud';
+
+ReactDOM.render(
+    <Tagcloud />,
+    document.getElementById('app')
+);
---
+++ tagcloud.js
@@ -0,0 +1,9 @@
+import React from 'react';
+ 
+class Tagcloud extends React.Component {
+  render() {
+    return <p>I am a Tag!! Obviously.</p>;
+  }
+}
+ 
+export default Tagcloud
